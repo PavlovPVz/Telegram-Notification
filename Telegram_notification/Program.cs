d2e0615eb6047cd3e476950f43738373a658acb0
PavlovPVz <57034381+PavlovPVz@users.noreply.github.com>
2021-05-10 17:15:47 +0300

    Add files via upload

---
+++ Telegram_notification/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using api;
+
+namespace Telegram_notification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tgtoken = "TOKEN";
+            string tgchatid = "USERID";
+            telegram.SendMessage("Telegram notification", tgtoken, tgchatid);
+        }
+    }
+}
---
+++ Telegram_notification/Telegram/api.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace api
+{
+    class telegram
+    {
+        public static bool SendMessage(string text, string tgtoken, string tgchatid)
+        {
+                using (WebClient c = new WebClient())
+                {
+                    string response = c.DownloadString(
+                        "https://api.telegram.org/bot" + tgtoken + "/sendMessage" +
+                        "?chat_id=" + tgchatid +
+                        "&text=" + text +
+                        "&parse_mode=Markdown" +
+                        "&disable_web_page_preview=True"
+                    );
+                    return !response.StartsWith("{\"ok\":false");
+                }
+        }
+    }
+}
